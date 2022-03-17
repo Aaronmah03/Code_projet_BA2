d5b41b600b902e1c2c42abfb42e746e259efbb27
Aaronmah03 <51461177+Aaronmah03@users.noreply.github.com>
2022-03-17 22:56:57 +0100

    Update Vérification_csv.py

--- Vérification_csv.py
+++ Vérification_csv.py
@@ -1,17 +1,14 @@
 #ATTENTION TJRS RETIRER LES 'XY' DE LA PREMIERE LIGNE DU CSV
 import matplotlib.pyplot as plt
 import numpy as np
-x,y= np.loadtxt('fichier.csv', delimiter=',', unpack=True)
-plt.plot(x,y, marker='.',markersize=1)
+nom=input()
+def neg(a):
+    if a=='B' or a=='H':
+        neg=1
+    else :
+        neg=-1
+    return neg
+x,y= np.loadtxt('csv_projet/'+nom+'.csv', delimiter=',', unpack=True)
+plt.plot(x,neg(nom)*y, marker='.',markersize=1)
 plt.title('Vérification du CSV')
 plt.show()
-\end{minted}
-Et celui écrit en matlab;
-\begin{minted}{matlab}
-data = csvread('fichier.csv'); % Read the data
-t = data(:,1) ; 
-x = data(:,2) ; 
-data
-figure('NumberTitle','off','Name','Vérification csv','Color','w')
-plot(t,x,marker='.',markersize=8)
-xlabel('csv')
